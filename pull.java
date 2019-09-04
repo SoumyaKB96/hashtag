fe62a5290af1d80460a2699fcf60e68a9d82b810
SoumyaKB96 <42297139+SoumyaKB96@users.noreply.github.com>
2019-09-04 16:39:21 +0530

    Add files via upload

--- pull.java
+++ pull.java
@@ -1,5 +1,5 @@
 package avc;
-import java.util.*;
+
 import java.io.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,15 +11,8 @@ public class pull {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner ob=new Scanner(System.in);
-		System.out.println("Enter the hour");
-		int n=ob.nextInt();
-		if(n>21) {System.out.println("enter vaid hour");n=0;}
-		else if(n>21)n=n+3;
-		else if(n>=9)n=n+2;
-		else if(n>=3)n++;
 		
-		System.out.println("Trending tweets \n");
+		
 				
 		
 
@@ -27,14 +20,13 @@ public class pull {
 		try {
 			
 			document = Jsoup.connect("https://trends24.in/india/").userAgent("Mozilla/17.0").get();
+			System.out.println("Trending tweets \n");
+			Element card = document.select("div.trend-card").first(); 
+			Elements info= card.select("a[href]");
+			for(int i=0;i<10;i++)System.out.println((i+1)+" " + info.get(i).text());
 			
-			Element cls = document.select("div.trend-card").get(n); 
-			Elements c2= cls.select("a[href]");
-			int k=1;
-			for(int i=0;i<10;i++)System.out.println((k++)+" " + c2.get(i).text());			
-			
-
-		}
+			} 
+		
 		catch (IOException e) {
 			e.printStackTrace();
 		}
